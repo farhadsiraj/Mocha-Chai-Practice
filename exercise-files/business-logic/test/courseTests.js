21c12a3136f25845fe4a348a9289dc925a26312c
Farhad Siraj <farhad.siraj@ibm.com>
2022-05-25 11:20:15 -0400

    Added tests for errors and  edge cases

--- exercise-files/business-logic/test/courseTests.js
+++ exercise-files/business-logic/test/courseTests.js
@@ -5,4 +5,73 @@ var Student = require("../Student"),
   chai = require("chai"),
   should = chai.should(),
   expect = chai.expect;
-describe("Course", function () {});
+
+describe("Course", function () {
+  const courseName = "Intro to Awesome",
+    courseCode = "Awe 101",
+    courseDescription = "This couse is awesome";
+  let student;
+
+  beforeEach(() => {
+    student = Student.create("John Doe", 5);
+  });
+
+  it("should save data correctly", () => {
+    const course = Course.create(courseName, courseCode, courseDescription);
+
+    should.exist(course.name);
+    should.exist(course.code);
+    should.exist(course.description);
+
+    should.exist(course.students);
+    course.students.should.eql([]);
+
+    should.exist(course.times);
+    course.times.should.eql([]);
+  });
+
+  describe("registerStudent", () => {
+    it("should add the student to the students array", () => {
+      const course = Course.create(courseName, courseCode, courseDescription);
+      course.registerStudent(student);
+
+      course.students.length.should.equal(1);
+      course.students[0].id.should.equal(student.id);
+    });
+  });
+
+  describe("unregisterStudent", () => {
+    it("should throw an error if we try to remove a student that isn't in the class", () => {
+      const course = Course.create(courseName, courseCode, courseDescription);
+
+      expect(() => {
+        course.unregisterStudent("test");
+      }).to.throw();
+    });
+  });
+
+  describe("addTimes", () => {
+    it("should add the given days/times to the course", () => {
+      const course = Course.create(courseName, courseCode, courseDescription);
+      const days = ["Monday", "Wednesday", "Friday"];
+      const times = ["10:00", "14:00"];
+
+      course.addTimes(days, times);
+      course.times.length.should.equal(6);
+      course.times[2].should.eql({
+        day: "Wednesday",
+        time: "10:00",
+      });
+    });
+
+    it("should not add a non-day to the times array", () => {
+      const course = Course.create(courseName, courseCode, courseDescription);
+      const days = "randomday";
+      const times = "10:00";
+
+      expect(() => {
+        course.addTimes(days, times);
+      }).to.throw();
+    });
+  });
+});
